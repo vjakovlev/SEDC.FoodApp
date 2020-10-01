fdd037a47810b91442d138df413ab88058890a9f
viktorjakovlev <viktorjakovlev@hotmail.com>
2020-10-01 13:42:13 +0200

    Project structur init

---
+++ SEDC.FoodApp.Server/SEDC.FoodApp.Web/Controllers/AliveController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SEDC.FoodApp.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AliveController : ControllerBase
+    {
+        [HttpGet("isalive")]
+        public IActionResult IsAlive() 
+        {
+            return Ok();
+        }
+    }
+}
